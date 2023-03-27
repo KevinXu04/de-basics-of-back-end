619e78c396c660f5cda442cb799e857a208a2461
Kevin <99073020@mydavinci.nl>
2023-03-27 14:12:22 +0200

    Back End 2 | Forms POST Methode

--- back-end-2/html-formulier/welcome.php
+++ back-end-2/html-formulier/welcome.php
@@ -2,8 +2,8 @@
 <body>
 
 <h1>De ingevulde gegevens zijn:</h1>
-Naam: <?php echo $_GET["naam"]; ?><br>
-Emailadres: <?php echo $_GET["email"]; ?>
+Naam: <?php echo $_POST["naam"]; ?><br>
+Emailadres: <?php echo $_POST["email"]; ?>
 
 </body>
 </html>
